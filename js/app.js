2d43b648d50dd6974ded99dd5958b57075252132
dina <dina.elghareeb@gamil.com>
2021-12-17 15:11:21 +0100

    submenu added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+import sublinks from './data.js';
+const linkBtns = [...document.querySelectorAll('.nav-link')];
+const submenukid = document.getElementById('submenu-kid');
+const submenuouting = document.getElementById('submenu-outing');
+const submenumore = document.getElementById('submenu-more');
 // ********** set date ************
 // select span
 const date = (document.getElementById(
@@ -7,61 +12,69 @@ const date = (document.getElementById(
 // ********** nav toggle ************
 // select button and links
 const navBtn = document.getElementById("nav-toggle");
-const links = document.getElementById("nav-links");
+const links = document.getElementById("menu");
 // add event listener
 navBtn.addEventListener("click", () => {
   links.classList.toggle("show-links");
 });
 
-// ********** nav kids toggle ************
-// select button and links
-const navkidsBtn = document.getElementById("nav-kids-toggle");
-const kids = document.getElementById("nav-kids");
-// add event listener
-navkidsBtn.addEventListener("mouseover", () => {
-  kids.classList.add("show-kids");
-});
+// ************************ submenu *****************************
+linkBtns.forEach((btn) => {
+  const text = btn.textContent;
+  let tempinnerHTML = '';
+  console.log("Hello world! --- show 1 " + text);
+  const tempPage = sublinks.find((link) => link.page === text);
 
-const home = document.getElementById("hero");
-home.addEventListener('mouseover', e => {
-  // prevent default
-  e.preventDefault();
-  kids.classList.remove('show-kids');
-});
-
-const navbar = document.getElementById("navbar");
-navbar.addEventListener('mouseover', e => {
-  // prevent default
-  e.preventDefault();
-  if (!e.target.classList.contains('nav-kid') && !e.target.classList.contains('icon-kid')) {
-    kids.classList.remove('show-kids');
+  if (tempPage) {
+    const { page, links } = tempPage;
+    tempinnerHTML = `     
+            ${links
+        .map((link) => {
+          return `<li><a class="sub-link" href="${link.url}"><i class="${link.icon} nav-icon"></i>${link.label}</a></li>`;
+        })
+        .join('')}
+      `;
+    console.log("Hello world! --- show 2 " + page);
+    if (page == "kids") {
+      console.log("Hello world! --- show in  " + page);
+      submenukid.innerHTML = tempinnerHTML;
+    }
+    else if (page == "Outings") {
+      submenuouting.innerHTML = tempinnerHTML;
+    }
+    else if (page == "More") {
+      submenumore.innerHTML = tempinnerHTML;
+    }
   }
 });
 
+// ********** ************ ************ ************ ************
+
+
 // ********** smooth scroll ************
 // select links
-const scrollLinks = document.querySelectorAll(".scroll-link");
-scrollLinks.forEach(link => {
-  link.addEventListener("click", e => {
-    // prevent default
-    e.preventDefault();
-    links.classList.remove("show-links");
-    kids.classList.remove('show-kids');
+// const scrollLinks = document.querySelectorAll(".nav-link");
+// scrollLinks.forEach(link => {
+//   link.addEventListener("click", e => {
+//     // prevent default
+//     e.preventDefault();
+//     links.classList.remove("show-links");
+//     // submenu.classList.remove('show');
 
-    const id = e.target.getAttribute("href").slice(1);
-    const element = document.getElementById(id);
-    //
-    console.log(element);
+//     const id = e.target.getAttribute("href").slice(1);
+//     const element = document.getElementById(id);
+//     //
+//     // console.log(element);
 
-    let position = element.offsetTop - 62;
-    console.log(position);
+//     let position = element.offsetTop - 62;
+//     // console.log(position);
 
-    window.scrollTo({
-      left: 0,
-      // top: element.offsetTop,
-      top: position,
-      behavior: "smooth"
-    });
-  });
-});
+//     window.scrollTo({
+//       left: 0,
+//       // top: element.offsetTop,
+//       top: position,
+//       behavior: "smooth"
+//     });
+//   });
+// });
 
---
+++ js/data.js
@@ -0,0 +1,42 @@
+const sublinks = [
+    {
+        page: 'kids',
+        links: [
+            { label: 'playground', icon: 'fas fa-parachute-box', url: 'tours-kids.html' },
+            { label: 'zoo', icon: 'fas fa-hippo', url: 'tours-kids.html' },
+            { label: 'swimming', icon: 'fas fa-swimmer', url: 'tours-kids.html' },
+            { label: 'museum', icon: 'fas fa-archway', url: 'tours-kids.html' },
+            { label: 'farm', icon: 'fab fa-pagelines', url: 'tours-kids.html' },
+            { label: 'circus', icon: 'fas fa-credit-card', url: 'tours-kids.html' },
+            // { label: 'cinma', icon: 'fas fa-credit-card', url: 'tours-kids.html' },
+            // { label: 'theater', icon: 'fas fa-credit-card', url: 'tours-kids.html' },
+            { label: 'mountains', icon: 'fas fa-credit-card', url: 'tours-kids.html' },
+        ],
+    },
+    {
+        page: 'Outings',
+        links: [
+            { label: 'parks', icon: 'fas fa-book', url: 'tours.html' },
+            { label: 'lakes', icon: 'fas fa-book', url: 'tours.html' },
+            { label: 'city', icon: 'fas fa-book', url: 'tours.html' },
+            { label: 'malls', icon: 'fas fa-book', url: 'tours.html' },
+            { label: 'shopping', icon: 'fas fa-book', url: 'tours.html' },
+            { label: 'restaurants', icon: 'fas fa-book', url: 'tours.html' },
+            { label: 'cafes', icon: 'fas fa-book', url: 'tours.html' },
+        ],
+    },
+    {
+        page: 'More',
+        links: [
+            { label: 'events', icon: 'fas fa-briefcase', url: 'tours.html' },
+            { label: 'gym', icon: 'fas fa-briefcase', url: 'tours.html' },
+            { label: 'markets', icon: 'fas fa-briefcase', url: 'tours.html' },
+            { label: 'furniture', icon: 'fas fa-briefcase', url: 'tours.html' },
+            { label: 'online shopping', icon: 'fas fa-briefcase', url: 'tours.html' },
+            { label: 'cakes', icon: 'fas fa-briefcase', url: 'tours.html' },
+            { label: 'arabic school', icon: 'fas fa-briefcase', url: 'tours.html' },
+        ],
+    },
+];
+
+export default sublinks;
