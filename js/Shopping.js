3f62ef9912f352618da3c5f8474a28955b46e371
dina <dina.elghareeb@gamil.com>
2022-05-06 17:34:59 +0200

    fix

--- js/Shopping.js
+++ js/Shopping.js
@@ -46,8 +46,9 @@ const displayshopping = () => {
         .map((item, index) => {
             const { id, title, category, icon, images, location, site, description, likes, featured, address, phone, rate, thingstodo, facebook } = item;
             return `<article class="item">
-        <div class="tour-img-container">
-            <img src="${images[0]}" class="item-photo" />           
+        <div class="tour-img-container">            
+            <a href="${location}" target="_blank">
+            <img src="${images[0]}" class="item-photo" />   </a>        
         </div>
         <div class="item-info">
         <header>            
